82c0295eaf9212a85929912f115e856771d11724
PNKishore01 <nagakishore3639@gmail.com>
2026-01-22 18:29:26 +0530

    Structure of a Java Program

---
+++ ProgramStructure.java
@@ -0,0 +1,26 @@
+class ProgramStructure{
+
+    //Block of statements
+
+    public static void main(String[] args){
+
+        //Block of statements
+
+        System.out.println("Hi, Welcome to the Java");
+    }
+}
+
+/**
+ * Class -> Predefined Keyword (Reserved Keyword for java)
+ * ProgramStructure - UserDefined DataType (It is also called java className)
+ * public -> Access Modifier (it can access upto JVM)
+ * static -> Non-Access Modifier 
+ * void -> Non-return type
+ * main -> methodName
+ * (String[] args) -> parameters
+ * System -> Predefined Class
+ * out -> variable or object
+ * println -> predefined method
+ * ("Hi, Welcome to the Java") -> user friendly message
+ * ";" -> semi-colon should mandatory for every end of statemnet
+ */
